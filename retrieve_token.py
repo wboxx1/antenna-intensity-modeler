4dadfeecb209f98495da579e3183e09b8d9aca0b
wboxx1 <wboxx1@gmail.com>
2020-04-30 17:10:14 -0500

    azure yml fix

--- retrieve_token.py
+++ retrieve_token.py
@@ -3,17 +3,14 @@ import subprocess
 import sys
 import configparser
 
-pyprc_path = os.path.expanduser("~/.pypirc")
-print("first: {}".format(pyprc_path))
 pyprc_path = os.environ["PYPIRC_PATH"]
-print("second: {}".format(pyprc_path))
+
 if os.path.exists(pyprc_path):
     print("Path exists.")
     cfg = configparser.ConfigParser()
     cfg.read(pyprc_path)
-    username = username or cfg.get("pypi", "username", fallback=None)
-    if not password:
-        password = cfg.get("pypi", "password", fallback=None)
-        os.environ["POETRY_PYPI_TOKEN_PYPI"] = password
+    username = cfg.get("pypi", "username", fallback=None)
+    password = cfg.get("pypi", "password", fallback=None)
+    os.environ["POETRY_PYPI_TOKEN_PYPI"] = password
 else:
     print("Couldn't find that shit.")

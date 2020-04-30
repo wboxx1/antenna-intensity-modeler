d7107c2d891a0d1b6907262e92a1302cc130be96
wboxx1 <wboxx1@gmail.com>
2020-04-30 15:06:33 -0500

    azure yml fix

---
+++ retrieve_token.py
@@ -0,0 +1,13 @@
+import os
+import subprocess
+import sys
+
+pyprc_path = os.path.expanduser("~/.pypirc")
+if os.path.exists(pyprc_path):
+    cfg.read(pyprc_path)
+    cfg = configparser.ConfigParser()
+    username = username or cfg.get("pypi", "username", fallback=None)
+    if not password:
+        password = cfg.get("pypi", "password", fallback=None)
+
+os.environ["POETRY_PYPI_TOKEN_PYPI"] = password
